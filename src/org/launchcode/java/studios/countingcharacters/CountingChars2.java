3cb4a097b2c1b38b78636102f80c61ee07bdc070
Lisa Ulricksen <LisaHome@lisas-imac.attlocal.net>
2022-03-21 20:31:19 -0500

    Chapter 3 Studio Counting Characters

---
+++ src/org/launchcode/java/studios/countingcharacters/CountingChars2.java
@@ -0,0 +1,36 @@
+package org.launchcode.java.studios.countingcharacters;
+
+import java.sql.SQLOutput;
+import java.util.Arrays;
+import java.util.HashMap;
+import java.util.Map;
+import java.util.Scanner;
+
+public class CountingChars2 {
+
+    public static void main(String[] args) {
+
+        HashMap<Character,Integer> CharCounterMap = new HashMap<>();
+        String string;
+        System.out.println("Please enter a string to search: ");
+            Scanner input = new Scanner(System.in);
+            string = input.nextLine().toLowerCase();
+
+        char[] stringToSearch = string.toCharArray();
+        //stringToSearch = Arrays.stream(stringToSearch).filter(x => Character.isLetter(x)).toArray();
+        for(char c : stringToSearch) {
+            if(Character.isLetter(c)) {
+                if(!CharCounterMap.containsKey(c)) {
+                    CharCounterMap.put(c, 1);
+                }else {
+                    Integer count = CharCounterMap.get(c) + 1;
+                    CharCounterMap.put(c, count);
+                }
+            }
+        }
+
+        for(Map.Entry<Character,Integer> character : CharCounterMap.entrySet()){
+            System.out.println(character.getKey() + ": " + character.getValue());
+        }
+    }
+}
