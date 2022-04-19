e4fd25c6286553789163c68e8e0af3b3b8762d00
Lisa Ulricksen <lisa.joshua1.9@gmail.com>
2022-04-19 17:59:37 -0500

    attempting Bonus to compare allergens, not succeeding yet

--- src/org/launchcode/java/demos/lsn7interfaces/FlavorComparator.java
+++ src/org/launchcode/java/demos/lsn7interfaces/FlavorComparator.java
@@ -4,9 +4,18 @@ import java.util.Comparator;
 
 public class FlavorComparator implements Comparator<Flavor> {
 
+//    @Override
+//    public int compare(Flavor o1, Flavor o2) {
+//        return o1.getName().compareTo(o2.getName());
+//    }
+
     @Override
     public int compare(Flavor o1, Flavor o2) {
-        return o1.getName().compareTo(o2.getName());
+        int allergenCount_o1 = o1.getAllergens().size();
+        int allergenCount_o2 = o2.getAllergens().size();
+        return o1.getAllergens().size().compareTo(o2.getAllergens().size());
+        return allergenCount_o1.compareTo(allergenCount_o2);
+
     }
 
 }
