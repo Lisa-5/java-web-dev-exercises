b26a011e713373607fac8140aa6dd0c3d6a8c250
Lisa Ulricksen <lisa.joshua1.9@gmail.com>
2022-04-20 20:16:19 -0500

    deleting comments from Inheritance Studio

--- src/org/launchcode/java/studios/funwithquizzes/MultipleChoice.java
+++ src/org/launchcode/java/studios/funwithquizzes/MultipleChoice.java
@@ -4,8 +4,6 @@ import java.util.Objects;
 
 public class MultipleChoice extends Question {
 
-    //private String test;
-
     public MultipleChoice(String prompt, String answer) {
         super(prompt, answer);
     }
--- src/org/launchcode/java/studios/funwithquizzes/QuizRunner.java
+++ src/org/launchcode/java/studios/funwithquizzes/QuizRunner.java
@@ -6,18 +6,21 @@ import java.util.Scanner;
 public class QuizRunner {
 
     public static void main(String[] args) {
+
         Question q1 = new MultipleChoice("""
                 What color are apples?
                 (a) red/green
                 (b) Orange
                 (c) Magenta
                 """, "a");
+
         Question q2 = new MultipleChoice("""
                 What color are bananas?
                 (a) red/green
                 (b) Yellow
                 (c) Blue
                 """, "b" );
+
         Question q3 = new TrueFalse("""
                 Is the earth is flat?" + "
                 True OR False""", "False");
@@ -28,6 +31,7 @@ public class QuizRunner {
         quiz1.addNewQuestion(q1);
         quiz1.addNewQuestion(q2);
 
+        quiz2.addNewQuestion(q1);
         quiz2.addNewQuestion(q3);
 
         quiz1.takeTest();
