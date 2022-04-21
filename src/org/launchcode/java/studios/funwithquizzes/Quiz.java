f54d663ee45413c9b48e3367a08e6a2adab8b1f5
Lisa Ulricksen <lisa.joshua1.9@gmail.com>
2022-04-20 20:06:58 -0500

    cleaned up Quiz.java in Inheritance Studio

--- src/org/launchcode/java/studios/funwithquizzes/Quiz.java
+++ src/org/launchcode/java/studios/funwithquizzes/Quiz.java
@@ -7,36 +7,11 @@ public class Quiz {
 
     private ArrayList<Question> questions = new ArrayList<>();
 
-    public Quiz(){
-//        Question q1 = new MultipleChoice("""
-//                What color are apples?
-//                (a) red/green
-//                (b) Orange
-//                (c) Magenta
-//                """, "a");
-//        Question q2 = new MultipleChoice("""
-//                What color are bananas?
-//                (a) red/green
-//                (b) Yellow
-//                (c) Blue
-//                """, "b" );
-//        Question q3 = new TrueFalse("""
-//                Is the earth is flat?" + "
-//                True OR False""", "False");
-////        Question q4 = new Checkbox("""
-////                Choose all animals with spots.
-////                (a) leopards
-////                (b) lady bugs
-////                (c) polar bears""" , "a", "b");
-//
-//        questions.add(q1);
-//        questions.add(q2);
-//        questions.add(q3);
-        //questions.add(q4);
+    public Quiz(){}
+
+    public void addNewQuestion(Question question){
+        this.questions.add(question);
     }
-        public void addNewQuestion(Question question){
-            this.questions.add(question);
-        }
 
     public void takeTest() {
         int score = 0;
